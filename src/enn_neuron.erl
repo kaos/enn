984b72e953864aadb962b302c3c2625099c50060
Andreas Stenius <git@astekk.se>
2012-03-06 11:13:30 +0100

    Refactored enn_neuron into a simple library.

--- src/enn_neuron.erl
+++ src/enn_neuron.erl
@@ -7,19 +7,12 @@
 %%%-------------------------------------------------------------------
 -module(enn_neuron).
 
--behaviour(gen_server).
-
 %% API
 -export([
-         start_link/1,
          new/1,
          input/2,
          step/1
         ]).
-
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -27,99 +20,24 @@
 
 -include("enn.hrl").
 
--record(state, {
-          neurons=[]
-         }).
 
 %%====================================================================
 %% API
 %%====================================================================
 %%--------------------------------------------------------------------
-%% Function: start_link(Args) -> {ok,Pid} | ignore | {error,Error}
-%% Description: Starts the server
+%% Function: new([#neuron{}]) -> term()
+%% Description: Create a neuron layer
 %%--------------------------------------------------------------------
-start_link(Args) ->
-    gen_server:start_link(?MODULE, Args, []).
-
 new(Args) ->
-    {ok, Ref} = start_link(Args),
-    Ref.
+    % todo: verify args
+    Args.
 
-input(Neuron, Input) ->
-    gen_server:call(Neuron, {input, Input}).
+input(Layer, Input) ->
+    [get_output(Neuron, Input) || Neuron <- Layer].
 
-step(Neuron) ->
-    gen_server:call(Neuron, step).
+step(Layer) ->
+    [0 || _ <- Layer].
 
-%%====================================================================
-%% gen_server callbacks
-%%====================================================================
-
-%%--------------------------------------------------------------------
-%% Function: init(Args) -> {ok, State} |
-%%                         {ok, State, Timeout} |
-%%                         ignore               |
-%%                         {stop, Reason}
-%% Description: Initiates the server
-%%--------------------------------------------------------------------
-init(Args) ->
-    {ok, #state{ 
-       neurons=Args
-      }
-    }.
-
-%%--------------------------------------------------------------------
-%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
-%%                                      {reply, Reply, State, Timeout} |
-%%                                      {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, Reply, State} |
-%%                                      {stop, Reason, State}
-%% Description: Handling call messages
-%%--------------------------------------------------------------------
-handle_call({input, Input}, _From, #state{neurons=L}=State) ->
-    Reply = [get_output(N, Input) || N <- L],
-    {reply, Reply, State};
-
-handle_call(step, _From, #state{neurons=L}=State) ->
-    % stepping a neuron gives no new output
-    % ... or, should it sustain the last output?
-    {reply, [0 || _ <- L], State}.
-
-%%--------------------------------------------------------------------
-%% Function: handle_cast(Msg, State) -> {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, State}
-%% Description: Handling cast messages
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: handle_info(Info, State) -> {noreply, State} |
-%%                                       {noreply, State, Timeout} |
-%%                                       {stop, Reason, State}
-%% Description: Handling all non call/cast messages
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: terminate(Reason, State) -> void()
-%% Description: This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any necessary
-%% cleaning up. When it returns, the gen_server terminates with Reason.
-%% The return value is ignored.
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% Description: Convert process state when code is changed
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
 
 %%--------------------------------------------------------------------
 %%% Internal functions
@@ -139,23 +57,23 @@ transfer_net_input({M, F}, N) ->
 -ifdef(TEST).
 
 single_neuron_bias_test() ->
-    {ok, N} = start_link([#neuron{ w=[0], b=321, f=purelin }]),
+    N = new([#neuron{ w=[0], b=321, f=purelin }]),
     ?assertEqual([321], input(N, [123])).
 
 single_neuron_weight_test() ->
-    {ok, N} = start_link([#neuron{ w=[1], b=0, f=fun enn_f:purelin/1 }]),
+    N = new([#neuron{ w=[1], b=0, f=fun enn_f:purelin/1 }]),
     ?assertEqual([123], input(N, [123])).
     
 single_neuron_multi_input_test() ->
-    {ok, N} = start_link([#neuron{ w=[1, 2, 3], b=0, f={enn_f,purelin} }]),
+    N = new([#neuron{ w=[1, 2, 3], b=0, f={enn_f,purelin} }]),
     ?assertEqual([28], input(N, [6, 5, 4])).
 
 single_neuron_multi_input_with_bias_test() ->
-    {ok, N} = start_link([#neuron{ w=[1.1, 2.2, 3.3], b=-10.5, f=purelin }]),
+    N = new([#neuron{ w=[1.1, 2.2, 3.3], b=-10.5, f=purelin }]),
     ?assertEqual([0.5], input(N, [3, 2, 1])).
 
 multiple_neuron_test() ->
-    {ok, N} = start_link([#neuron{w=[1, 2], b=3, f=purelin}, #neuron{w=[4, 5], b=6, f=purelin}]),
+    N = new([#neuron{w=[1, 2], b=3, f=purelin}, #neuron{w=[4, 5], b=6, f=purelin}]),
     ?assertEqual([26, 7*4+8*5+6], input(N, [7, 8])).
 
 'P2.3_test'() ->
